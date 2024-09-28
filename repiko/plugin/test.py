ed1fa8d857813adc1a976dfc16df9f24ed61076c
liggest <liggest@sina.cn>
2024-09-29 00:04:42 +0900

    整理 test 代码

--- repiko/plugin/test.py
+++ repiko/plugin/test.py
@@ -95,25 +95,20 @@ def pr2str(pr: ParseResult):
         str(pr.raw),
         f"类型：{repr(pr.type) if pr.type else '未知'}",
         f"指令：{repr(pr.command) if pr.command else '未知'}",
-        f"参数：{repr(pr.params)}",
+        f"参数：{repr(pr.params)}"
     ]
     if pr.hasOpt():
         result.extend(f"{k}: {v}" for k, v in pr.args.items())
     return "\n".join(result).strip()
 
-
 Command("-error").opt("-api", OPT.N, "ApiError")
-
-
 @Events.onCmd("error")
 async def error(pr: ParseResult):
     if pr["api"]:
         raise ApiError({"retcode": -1, "msg": "test"})
     else:
         from httpx import TimeoutException
-
         raise TimeoutException("test")
-
 
 @Events.onExecuteError
 async def pasteErrorEmoji(pr: ParseResult, parser: CommandParser, e: Exception):
